2757a3898b50445a21ee8a1f76482811a580ab6f
Elena Yan <elena.yan@studio.unibo.it>
2023-01-30 11:34:51 +0100

    test: basic test

---
+++ src/test/java/it/unibo/smartgh/Test.java
@@ -0,0 +1,11 @@
+package it.unibo.smartgh;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    public void test() {
+        assertTrue(true);
+    }
+}
